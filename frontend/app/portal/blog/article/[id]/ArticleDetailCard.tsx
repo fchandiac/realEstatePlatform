1e2827dc5d8c2cb7becc8ba54fa2dad6837591b2
Felipe Chandía <felipe.chandia.cast@gmail.com>
2025-11-13 11:37:45 -0300

    style: Adjust article hero image height to h-90

--- frontend/app/portal/blog/article/[id]/ArticleDetailCard.tsx
+++ frontend/app/portal/blog/article/[id]/ArticleDetailCard.tsx
@@ -25,7 +25,7 @@ export default function ArticleDetailCard({
   return (
     <article className="w-full bg-white">
       {/* Imagen Hero */}
-      <div className="relative w-full h-80 overflow-hidden bg-gray-200">
+      <div className="relative w-full h-90 overflow-hidden bg-gray-200">
         <img
           src={article.multimediaUrl || fallbackImage}
           alt={article.title}
